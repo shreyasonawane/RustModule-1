f8282825e417157eecef35c487f3c5ab02a92010
Shreya Sonawane <94279051+shreyasonawane@users.noreply.github.com>
2022-12-29 09:46:27 +0530

    Update fc1.rs

--- module1/src/bin/fc1.rs
+++ module1/src/bin/fc1.rs
@@ -10,6 +10,13 @@
 // * Use an if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+let x:bool=true;
+  if x==true{
+    println!("hello");
+  }
+  else{
+    println!("goodbye");
+}
 
 
